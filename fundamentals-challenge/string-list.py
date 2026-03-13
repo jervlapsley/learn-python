96f5fc7ae7920ebf44b08655013becb88ae146f9
jervlapsley <80655164+jervlapsley@users.noreply.github.com>
2026-03-13 16:28:08 -0700

    completed strings-list

--- fundamentals-challenge/string-list.py
+++ fundamentals-challenge/string-list.py
@@ -1,1 +1,11 @@
-palindrome = input("Check if your word is a palindrome! Enter a word: ")
+palindrome = input("Check if your word is a palindrome! \nEnter a word: ")
+palindromeChecker = palindrome[::-1]
+
+if palindrome == palindromeChecker:
+    print(f"Your word '{palindrome}' is a palindrome!")
+else:
+    print("Nope, try again!")
+
+
+
+    
